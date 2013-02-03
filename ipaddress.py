03013367a16ed775a1f26bd653af934e7dc67c11
Philipp Hagemeister <phihag@phihag.de>
2013-02-04 00:49:04 +0100

    strict struct.pack for 2.6

--- ipaddress.py
+++ ipaddress.py
@@ -43,8 +43,12 @@ def _compat_to_bytes(intval, length, endianess):
     assert isinstance(intval, _compat_int_types)
     assert endianess == 'big'
     if length == 4:
+        if intval < 0 or intval >= 2**32:
+            raise struct.error("integer out of range for 'I' format code")
         return struct.pack('!I', intval)
     elif length == 16:
+        if intval < 0 or intval >= 2**128:
+            raise struct.error("integer out of range for 'QQ' format code")
         return struct.pack('!QQ', intval >> 64, intval & 0xffffffffffffffff)
     else:
         raise NotImplementedError()
