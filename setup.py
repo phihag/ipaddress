3b2e054c5db6ae7c004bbc8c7cc42096f1f75972
Philipp Hagemeister <phihag@phihag.de>
2014-11-24 02:40:36 +0100

    Correct setup.py tags

--- setup.py
+++ setup.py
@@ -23,8 +23,8 @@ settings = {
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2'
-        'Programming Language :: Python :: 3.3'
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ),
     'py_modules': ['ipaddress']
 }
